aaf03320be4b31d9fab6407e7d8c46297c8309d3
unknown <km18acb@herts.ac.uk>
2019-01-02 02:57:10 +0800

    Angular service & filter functions implemented. To Resolve: 304 error upon GET request to API. To try angular.extend() for outputSentiment mixin after critical issues are resolved

--- app_api/models/ideas.js
+++ app_api/models/ideas.js
@@ -26,9 +26,15 @@ var ideaSchema = new mongoose.Schema ({
 mongoose.model('Ideas', ideaSchema, 'wscIdeas');
 
 // Sample document
-/* db.wscIdeas.save({
-title: "Apple's Sales Performance Disappoints",
-ticker: 'AAPL',
-sentiment: 'Neutral',
-body: "Underwhelming sales for core products iPhone XS/XR, but revenue growth from services on track with expectations. A full-blown trade war will likely threaten core business"
-}) */
+/* 
+db.wscIdeas.save({
+  title: 'Attractive Valuation Post-Correction',
+  ticker: 'AAPL',
+  sentiment: 'Bullish',
+  text: 'In the aftermath of a major correction in the major indexes, tech stocks have been rattled by negative sentiment. A lacklustre quarter for Apple further subjected it to the wrath of a major selloff. As the dust begins to settle, I see a very good opportunity to once again invest in Apple, which should rebound swiftly given its robust product line, innovation, and gradual shift in focus towards its services segment with higher retention rates and commendable gross margins.',
+  postedBy: 'Jdoe1',
+  postedAt: '1 Jan 2019',
+  upvoteCount: '9',
+  downvoteCount: '3',
+})
+*/
--- public/angular/wscApp.js
+++ public/angular/wscApp.js
@@ -1,29 +1,49 @@
 angular.module('wscApp', []);
 
-var ideaListCtrl = function ($scope) {
-    $scope.data = {
-        ideas: [{
-            title: 'Attractive Valuation Post-Correction',
-            ticker: 'AAPL',
-            sentiment: 'Bullish',
-            text: 'In the aftermath of a major correction in the major indexes, tech stocks have been rattled by negative sentiment. A lacklustre quarter for Apple further subjected it to the wrath of a major selloff. As the dust begins to settle, I see a very good opportunity to once again invest in Apple, which should rebound swiftly given its robust product line, innovation, and gradual shift in focus towards its services segment with higher retention rates and commendable gross margins.',
-            postedBy: 'Jdoe1',
-            postedAt: '1 Jan 2019',
-            upvoteCount: '9',
-            downvoteCount: '3',
-        },{
-            title: 'The Struggle Against Generics',
-            ticker: 'PFE',
-            sentiment: 'Neutral',
-            text: 'After an impressive 2018 performance, Pfizer once again faces its decades-old nemesis: generic drugs. With the imminent expiry of patents covering some of its most valuable drugs in its pipeline, it is unclear whether Pfizer has what it takes to ramp up innovation and preserve a competitive moat to enhance its revenue stream. For now, it is difficult to find any further upside from its current valuation.',
-            postedBy: 'Marcus_Wright',
-            postedAt: '30 Dec 2018',
-            upvoteCount: '6',
-            downvoteCount: '7',
-        }]
+var ideaListCtrl = function ($scope, wscData) {
+    wscData
+        .success(function(data) {
+            $scope.data = { ideas: data };
+        })
+        .error(function (e) {
+            console.log(e);
+        });
+};
+
+
+var wscData = function ($http) {
+    return $http.get('/api/ideas');
+};
+
+/*
+var wscStaticData = function () {
+    return [{
+        title: 'Attractive Valuation Post-Correction',
+        ticker: 'AAPL',
+        sentiment: 'Bullish',
+        text: 'In the aftermath of a major correction in the major indexes, tech stocks have been rattled by negative sentiment. A lacklustre quarter for Apple further subjected it to the wrath of a major selloff. As the dust begins to settle, I see a very good opportunity to once again invest in Apple, which should rebound swiftly given its robust product line, innovation, and gradual shift in focus towards its services segment with higher retention rates and commendable gross margins.',
+        postedBy: 'Jdoe1',
+        postedAt: '1 Jan 2019',
+        upvoteCount: '9',
+        downvoteCount: '3'
+      }];
+};
+*/
+
+/*
+var TO_FIX_outputSentiment = function () {
+    return {
+        scope: {
+            thisSentiment : '=sentiment'
+        },
+        templateUrl: '/angular/output-sentiment.html'
     };
 };
+*/
 
 angular
     .module('wscApp')
-    .controller('ideaListCtrl', ideaListCtrl);
+    .controller('ideaListCtrl', ideaListCtrl)
+    .service('wscData', wscData)
+//  .directive('TO_FIX_outputSentiment', outputSentiment);
+    
