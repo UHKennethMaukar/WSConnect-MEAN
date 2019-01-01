436f3316a8621bf0aaf9ee188898b1f0eefdc45f
Kenneth M <km18acb@herts.ac.uk>
2019-01-01 16:40:56 +0800

    Angular implemented for Forum view and controller. To Do: Replace hard-coded data in controller with fetched data from API

--- app_server/controllers/forum.js
+++ app_server/controllers/forum.js
@@ -1,4 +1,15 @@
 /* Landing Page */
 module.exports.mainList = function (req, res) {
-    res.render('index', { title: 'Forum' });
+    renderForumpage(req, res);
   };
+
+var renderForumpage = function(req, res){
+  res.render('forum-list', {
+    title:'WSC - Connecting Investors Worldwide',
+    pageheader: {
+      title: 'WSC',
+      strapline:'View global sentiments, share your investment ideas'
+    },
+    sidebar:"Collaborate with worldwide investors and analysts in sharing your outlook for equity markets and companies. Valuations move with overall sentiment; your voice matters too"
+  });
+};
